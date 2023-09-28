041e3fe8d65d33b97cca77ed53f31a949527fcf3
wanghangit <724690450@qq.com>
2023-09-29 01:09:22 +0800

    fix build

--- packages/vue-schedule-picker/vite.config.ts
+++ packages/vue-schedule-picker/vite.config.ts
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import VitePluginStyleInject from 'vite-plugin-style-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dts({
+      entryRoot: "./src",
+      outputDir: ["dist"],
+      //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+      tsConfigFilePath: "../../tsconfig.json",
+    }),
+    VitePluginStyleInject()
   ],
   build: {
     lib: {
@@ -14,19 +23,20 @@ export default defineConfig({
       entry: resolve(__dirname, './src/index.ts'),
       name: 'Schedule',
       // the proper extensions will be added
-      fileName: 'dist/index.js'
+      fileName: 'index'
     },
+    cssCodeSplit: true,
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
-      output: {
+      output: [{
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
-        format: 'cjs',
-        exports: 'named',
-      },
+        entryFileNames: '[name].js',
+        format: 'es',
+      }],
     },
   },
   resolve: {
